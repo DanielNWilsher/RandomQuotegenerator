040762c1217b68f222d24fd80e8ab8ae3cf7b845
DanielNWilsher <daniel.m.wilsher@gmail.com>
2023-10-26 21:27:44 -0600

    more additions

--- Random quote generator.py
+++ Random quote generator.py
@@ -1,2 +1,64 @@
-#Random quote generator
+"""
+Random Quote Generator Script 
+Start Time: 9:14pm
+Finish Time:
 
+Objective: Create a Random Quote Generator with as much creativity as possible.
+
+
+"""
+import random
+import pytest
+
+
+noun_list = [
+    "Ability", "Access", "Accident", "Account", "Act", "Action", "Activity", "Actor", "Ad", "Addition",
+    "Address", "Administration", "Advantage", "Advertising", "Advice", "Affair", "Age", "Agency", "Agreement",
+    "Air", "Airport", "Alcohol", "Ambition", "Amount", "Analysis", "Analyst", "Animal", "Answer", "Anxiety",
+    "Apartment", "Appearance", "Apple", "Application", "Appointment", "Area", "Argument", "Army", "Arrival",
+    "Art", "Article", "Aspect", "Assignment", "Assistance", "Assistant", "Association", "Assumption", "Atmosphere",
+    "Attempt", "Attention", "Attitude", "Audience", "Aunt", "Average", "Awareness"
+]
+
+
+verb_list = [
+    "Accept", "Ache", "Acknowledge", "Act", "Add", "Admire", "Admit", "Admonish", "Adopt", "Advise",
+    "Affirm", "Afford", "Agree", "Ail", "Alert", "Allege", "Allow", "Allude", "Amuse", "Analyze",
+    "Announce", "Annoy", "Answer", "Apologize", "Appeal", "Appear", "Applaud", "Appreciate", "Approve", "Argue",
+    "Arrange", "Arrest", "Arrive", "Articulate", "Ask", "Assert", "Assure", "Attach", "Attack", "Attempt",
+    "Attend", "Attract", "Auction", "Avoid", "Avow"
+]
+
+adjective_list = [
+    "attractive", "bald", "beautiful", "chubby", "clean", "dazzling", "drab", "elegant", "fancy", "fit",
+    "flabby", "glamorous", "gorgeous", "handsome", "long", "magnificent", "muscular", "plain", "plump", "quaint",
+    "scruffy"
+]
+
+
+"""
+Mad Lib 1: The Great Adventure
+
+Once upon a time, in a [adjective] land, there lived a brave [noun].
+This [noun] was known far and wide for their [adjective] [plural noun]. One day, they embarked on a [adjective] adventure to find the legendary [noun]. 
+Along the way, they encountered a [adjective] [animal], who offered to be their [adjective] companion. Together, they faced many [adjective] challenges and overcame [number] obstacles. 
+In the end, they found the [noun], and it was [adjective] beyond their wildest dreams.
+
+Mad Lib 2: The Surprise Party
+
+It was [day of the week], and [name] was planning a surprise party for [friend's name]. [Name] secretly invited [number] [adjective] friends to the party and bought a [color] [noun] as a gift. They decorated the [adjective] [place] with [plural noun] and [adjective] streamers. [Friend's name] was [adjective] surprised when they walked into the room and saw their friends yelling, "Happy [adjective] birthday!" The party was a [adjective] success, and everyone had a [adjective] time.
+
+Mad Lib 3: The Haunted House
+
+One [season], a group of [number] [adjective] friends decided to visit a [adjective] haunted house. 
+As they entered, they heard [spooky sound] coming from the [room]. The [adjective] walls were covered in [noun], and there were [number] [adjective] [animal] roaming around. 
+They cautiously made their way to the [room] where they found a [color] [noun]. 
+Suddenly, the [noun] started [verb ending in -ing], and they all screamed and ran out of the house as fast as their [body part] could carry them.
+
+"""
+
+def main():
+    return True
+
+if __name__ == "__main__":
+    main()
